d6e830d48200ab509e04f9e4fc95200613ef7aea
Steve Green <steve@greengob.com>
2024-02-26 19:22:56 +0000

    day 3-1 progress

--- day_3_1/main.go
+++ day_3_1/main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
-type allCharacters map[string]string
-type numberCoordinates map[string]string
+const symbols = "!@#$%^&*()/+=-_?,;:[]{}`~£¢∞§¶•ªº™¡–≠"
+
+type numbers map[string]string
 
 func main() {
 	file, err := os.Open("./engineSchematic.txt")
@@ -18,34 +20,84 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	all := allCharacters{}
-	n := numberCoordinates{}
+	//n := numbers{}
 
-	//var res int
-	var lineNum int
+	var lines []string
+	found := 0
+	finalNumber := ""
+	var fuckingMatch int
 
 	for scanner.Scan() {
-		_ = processSchematic(scanner.Text(), all, n, lineNum)
-		lineNum++
-		//fmt.Print(res, " + ", val, " = ")
-		//res += val
-		//fmt.Print(res, "\n")
+		lines = append(lines, scanner.Text())
 	}
-	fmt.Print("=========\n", all, "\n")
-	fmt.Print("=========\n", n, "\n")
-	//fmt.Print("=========\n", res, "\n")
+	totalRows := len(lines)
+	for row := 0; row < totalRows; row++ {
+		for col := 0; col < len(lines[row]); col++ {
+			c := string(lines[row][col])
+			if _, err := strconv.Atoi(c); err == nil {
+				found++
+			} else {
+				for ; found > 0; found-- {
+					finalNumber += string(lines[row][col-found])
+				}
+				for i := 0; i < len(finalNumber); i++ {
+					// check the diagonal values around each number, and determine the final value
+					//n[strconv.Itoa(row)+"-"+strconv.Itoa(col-i)] = finalNumber
+					if checkDiagonals(row, col-i-1, lines, totalRows-1, len(lines[row])-1) {
+						f, _ := strconv.Atoi(finalNumber)
+						fmt.Println(fuckingMatch, "+", f, "=", fuckingMatch+f)
+						fmt.Println("row", row)
+						fuckingMatch += f
+					}
+				}
+				finalNumber = ""
+			}
+		}
+	}
+	fmt.Print("=========\n", fuckingMatch, "\n")
 }
 
-func processSchematic(line string, all allCharacters, n numberCoordinates, lineNum int) int {
-	for rowNum, char := range line {
-		c := string(char)
-		all[strconv.Itoa(lineNum)+strconv.Itoa(rowNum)] = c
-		if _, err := strconv.Atoi(c); err == nil {
-			// if its a number then store the coordinates
-			n[strconv.Itoa(lineNum)+strconv.Itoa(rowNum)] = c
-		}
-		if all[lineNum] {
+func checkDiagonals(row int, col int, lines []string, rowLength int, colLength int) bool {
+	if row > 0 && col > 0 {
+		leftUpper := string(lines[row-1][col-1])
+		fmt.Println("leftUpper", leftUpper)
+		if strings.Contains(symbols, leftUpper) {
+			return true
 		}
 	}
-	return 0
+	if row > 0 && col < colLength {
+		rightUpper := string(lines[row-1][col+1])
+		fmt.Println("rightUpper", rightUpper)
+		if strings.Contains(symbols, rightUpper) {
+			return true
+		}
+	}
+	if row < rowLength && col > 0 {
+		leftLower := string(lines[row+1][col-1])
+		fmt.Println("leftLower", leftLower)
+		if strings.Contains(symbols, leftLower) {
+			return true
+		}
+	}
+	if row < rowLength && col < colLength {
+		rightLower := string(lines[row+1][col+1])
+		fmt.Println("rightLower", rightLower)
+		if strings.Contains(symbols, rightLower) {
+			return true
+		}
+	}
+	// this only needs to happen on the first and last digits, but whatever
+	if col > 0 {
+		left := string(lines[row][col-1])
+		if strings.Contains(symbols, left) {
+			return true
+		}
+	}
+	if col < colLength {
+		right := string(lines[row][col+1])
+		if strings.Contains(symbols, right) {
+			return true
+		}
+	}
+	return false
 }
